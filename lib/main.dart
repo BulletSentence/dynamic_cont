657b08aef32d9e6008233eb038089f6175977c9c
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-25 11:25:08 -0300

    text for main

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "DNC",
-    home: Container(
-      color: Colors.white,
+    home: Column (
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "PPL: 0",
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "ENTER",
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.white,
+          ),
+        ),
+      ],
     ),
   ));
 }
